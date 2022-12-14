b7ff6ecd9ac3056966bf1bd243879d9716d28e60
EC2 Default User <ec2-user@ip-172-31-43-148.ec2.internal>
2022-12-14 07:22:25 +0000

    非ログインTopPageとログイン後TopPageを作成

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+
+class TaskController extends Controller {
+/**
+     * トップページ を表示する
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function list()
+    {
+        return view('task.list');
+    }
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>ログイン機能付きタスク管理サービス</title>
-    </head>
-    <body>
-        <h1>ログイン</h1>
-        <form action="login" method="post">
-            email：<input><br>
-            パスワード：<input type="password" name="password"><br>
-            <button>ログインする</button>
-        </form>
-    </body>
-</html>
+@extends('layout')
+
+{{-- メインコンテンツ --}}
+@section('contets')
+    <h1>ログイン</h1>
+    <form action="login" method="post">
+        email：<input><br>
+        パスワード：<input type="password" name="password"><br>
+        <button>ログインする</button>
+    </form>
+@endsection
